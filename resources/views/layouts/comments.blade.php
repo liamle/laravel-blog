c3cb465c5b6d2491cb54ef2558889b54e15cfda8
Tuan <tuan@leadsgen.asia>
2018-11-06 10:11:53 +0700

    set up blog layouts

---
+++ resources/views/layouts/comments.blade.php
@@ -0,0 +1,106 @@
+<article class="post-comments">
+    <h3><i class="fa fa-comments"></i> 5 Comments</h3>
+
+    <div class="comment-body padding-10">
+        <ul class="comments-list">
+            <li class="comment-item">
+                <div class="comment-heading clearfix">
+                    <div class="comment-author-meta">
+                        <h4>John Doe <small>January 14, 2016</small></h4>
+                    </div>
+                </div>
+                <div class="comment-content">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio nesciunt nulla est, atque ratione nostrum cumque ducimus maxime, amet enim tempore ipsam. Id ea, veniam ipsam perspiciatis assumenda magnam doloribus!</p>
+                    <p>Quibusdam iusto culpa, necessitatibus, libero sequi quae commodi ea ab non facilis enim vitae inventore laborum hic unde esse debitis. Adipisci nostrum reprehenderit explicabo, non molestias aliquid quibusdam tempore. Vel.</p>
+                </div>
+            </li>
+
+            <li class="comment-item">
+                <div class="comment-heading clearfix">
+                    <div class="comment-author-meta">
+                        <h4>John Doe <small>January 14, 2016</small></h4>
+                    </div>
+                </div>
+                <div class="comment-content">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio nesciunt nulla est, atque ratione nostrum cumque ducimus maxime, amet enim tempore ipsam. Id ea, veniam ipsam perspiciatis assumenda magnam doloribus!</p>
+                    <p>Quibusdam iusto culpa, necessitatibus, libero sequi quae commodi ea ab non facilis enim vitae inventore laborum hic unde esse debitis. Adipisci nostrum reprehenderit explicabo, non molestias aliquid quibusdam tempore. Vel.</p>
+
+                    <ul class="comments-list-children">
+                        <li class="comment-item">
+                            <div class="comment-heading clearfix">
+                                <div class="comment-author-meta">
+                                    <h4>John Doe <small>January 14, 2016</small></h4>
+                                </div>
+                            </div>
+                            <div class="comment-content">
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio nesciunt nulla est, atque ratione nostrum cumque ducimus maxime, amet enim tempore ipsam. Id ea, veniam ipsam perspiciatis assumenda magnam doloribus!</p>
+                                <p>Quibusdam iusto culpa, necessitatibus, libero sequi quae commodi ea ab non facilis enim vitae inventore laborum hic unde esse debitis. Adipisci nostrum reprehenderit explicabo, non molestias aliquid quibusdam tempore. Vel.</p>
+                            </div>
+                        </li>
+
+                        <li class="comment-item">
+                            <div class="comment-heading clearfix">
+                                <div class="comment-author-meta">
+                                    <h4>John Doe <small>January 14, 2016</small></h4>
+                                </div>
+                            </div>
+                            <div class="comment-content">
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio nesciunt nulla est, atque ratione nostrum cumque ducimus maxime, amet enim tempore ipsam. Id ea, veniam ipsam perspiciatis assumenda magnam doloribus!</p>
+                                <p>Quibusdam iusto culpa, necessitatibus, libero sequi quae commodi ea ab non facilis enim vitae inventore laborum hic unde esse debitis. Adipisci nostrum reprehenderit explicabo, non molestias aliquid quibusdam tempore. Vel.</p>
+
+                                <ul class="comments-list-children">
+                                    <li class="comment-item">
+                                        <div class="comment-heading clearfix">
+                                            <div class="comment-author-meta">
+                                                <h4>John Doe <small>January 14, 2016</small></h4>
+                                            </div>
+                                        </div>
+                                        <div class="comment-content">
+                                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio nesciunt nulla est, atque ratione nostrum cumque ducimus maxime, amet enim tempore ipsam. Id ea, veniam ipsam perspiciatis assumenda magnam doloribus!</p>
+                                            <p>Quibusdam iusto culpa, necessitatibus, libero sequi quae commodi ea ab non facilis enim vitae inventore laborum hic unde esse debitis. Adipisci nostrum reprehenderit explicabo, non molestias aliquid quibusdam tempore. Vel.</p>
+                                        </div>
+                                    </li>
+                                </ul>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </li>
+        </ul>
+
+    </div>
+
+    <div class="comment-footer padding-10">
+        <h3>Leave a comment</h3>
+        <form>
+            <div class="form-group required">
+                <label for="name">Name</label>
+                <input type="text" name="name" id="name" class="form-control" style="background-image: url(&quot;data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAABHklEQVQ4EaVTO26DQBD1ohQWaS2lg9JybZ+AK7hNwx2oIoVf4UPQ0Lj1FdKktevIpel8AKNUkDcWMxpgSaIEaTVv3sx7uztiTdu2s/98DywOw3Dued4Who/M2aIx5lZV1aEsy0+qiwHELyi+Ytl0PQ69SxAxkWIA4RMRTdNsKE59juMcuZd6xIAFeZ6fGCdJ8kY4y7KAuTRNGd7jyEBXsdOPE3a0QGPsniOnnYMO67LgSQN9T41F2QGrQRRFCwyzoIF2qyBuKKbcOgPXdVeY9rMWgNsjf9ccYesJhk3f5dYT1HX9gR0LLQR30TnjkUEcx2uIuS4RnI+aj6sJR0AM8AaumPaM/rRehyWhXqbFAA9kh3/8/NvHxAYGAsZ/il8IalkCLBfNVAAAAABJRU5ErkJggg==&quot;); background-repeat: no-repeat; background-attachment: scroll; background-size: 16px 18px; background-position: 98% 50%; cursor: auto;">
+            </div>
+            <div class="form-group required">
+                <label for="email">Email</label>
+                <input type="text" name="email" id="email" class="form-control">
+            </div>
+            <div class="form-group">
+                <label for="website">Website</label>
+                <input type="text" name="website" id="website" class="form-control">
+            </div>
+            <div class="form-group required">
+                <label for="comment">Comment</label>
+                <textarea name="comment" id="comment" rows="6" class="form-control"></textarea>
+            </div>
+            <div class="clearfix">
+                <div class="pull-left">
+                    <button type="submit" class="btn btn-lg btn-success">Submit</button>
+                </div>
+                <div class="pull-right">
+                    <p class="text-muted">
+                        <span class="required">*</span>
+                        <em>Indicates required fields</em>
+                    </p>
+                </div>
+            </div>
+        </form>
+    </div>
+
+</article>
